5e94c49372cf502fa9014cd3aa902fe77ccb4bcf
Angelina Wong <angelina.w25@gmail.com>
2018-05-02 23:22:24 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,14 @@
 def unsafe?(speed)
-	speed > 60 ? true : false
+	if speed > 60
+		return false
+	elsif speed < 40
+		return false
+	else
+		return true
 end
 
 
 
 def not_safe?(speed)
-
+	speed > 60 ? true : false
 end
